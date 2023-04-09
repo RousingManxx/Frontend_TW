01970a678a51f5f46f1e65361f1c8bed4b9b7289
Rousing <zs20018188@estudiantes.uv.mx>
2023-04-09 16:04:52 -0600

    FrontEnd

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+<script>
+    function edificio(id){
+        if (id == "antiguo"){
+            $("#antiguo").show();
+            $("#nuevo").hide();
+        }
+        if (id == "nuevo"){
+            $("#antiguo").hide();
+            $("#nuevo").show();
+        }
+    }
+
+    function aula(id){
+        if (id == "101"){
+            $("#101").show;
+        }
+    }
+</script>
